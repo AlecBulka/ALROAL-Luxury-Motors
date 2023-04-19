fd640287133ce8f8b061961c0c709ba4ab078c01
RogerSanchez-2DAW <r.sanchez2dawnuria2022@gmail.com>
2023-04-19 15:57:36 +0200

    create car model and migration

---
+++ database/migrations/2023_04_19_133015_create_cars_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('horsepower');
+            $table->string('torque');
+            $table->float('consumption');
+            $table->integer('topSpeed');
+            $table->float('acceleration');
+            $table->string('motor');
+            $table->string('wheels');
+            $table->string('tires');
+            $table->string('driveTrain');
+            $table->string('gearbox');
+            $table->string('exterior');
+            $table->string('interior');
+            $table->integer('kilometers');
+            $table->float('price');
+            $table->foreignId('models_id');
+            $table->foreignId('brands_id');
+            $table->integer('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
