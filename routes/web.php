06935e4ee1c1660097f20c0ae304ab8fac5a1bc7
RogerSanchez-2DAW <r.sanchez2dawnuria2022@gmail.com>
2023-04-24 16:37:22 +0200

    User Dashboard

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/user-dashboard', function () {
+    return view('user/dashboard');
+});
+
 require __DIR__.'/auth.php';
