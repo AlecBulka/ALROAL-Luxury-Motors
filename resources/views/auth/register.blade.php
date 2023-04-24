f35131510c4a49a6a4ebc20378d8a5b2d1d30165
alexguty42 <a.gutierrez2dawnuria2022@gmail.com>
2023-04-24 16:45:31 +0200

    pagina-principal register login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,62 +11,30 @@
     <link rel="stylesheet" href={{ asset('style/register/style.css') }}>
 </head>
 <body>
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+    <x-header/>
+    <div class="reg">
+        <form method="POST" id="form" action="{{ route('register') }}">
+        <h2>Register</h2>
+        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" placeholder="Name" :value="old('name')" required autofocus autocomplete="name" /> <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <x-text-input id="email" class="block mt-1 w-full" placeholder="Email" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Phone Number -->
-        <div class="mt-4">
-            <x-input-label for="phoneNumber" :value="__('Phone Number')" />
-            <x-text-input id="phoneNumber" class="block mt-1 w-full" type="tel" name="phoneNumber" :value="old('phoneNumber')" required autocomplete="phoneNumber" />
-            <x-input-error :messages="$errors->get('phoneNumber')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" placeholder="Password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+            <x-text-input id="password_confirmation" placeholder="Repeat Password" class="block mt-1 w-full"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
+        <label for=""><input type="text" placeholder="Phone Number"></label>
+        <label for=""><input type="submit" placeholder="Register" value="Register"></label>
+        <p class="p0">Alredy registered?</p>
+        <a href="{{ route('login') }}">Login</a>
+    </form>
+</div>
+    <x-footer/>
 </body>
 </html>
