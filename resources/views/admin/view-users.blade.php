102d73f6432ee9ec28d06594bcf98c2593af549c
Alec Bulka <a.bulka2dawnuria2022@gmail.com>
2023-04-26 15:56:40 +0000

    admin view users table

--- resources/views/admin/view-users.blade.php
+++ resources/views/admin/view-users.blade.php
@@ -35,6 +35,111 @@
             <a href="#">Buy Cars</a>
         </div>
     </div>
+
+    <div class="container">
+        <h1 class="title">Users List</h1>
+        <table class="table">
+            <thead class="thead">
+                <tr>
+                    <th scope="col" class="th">
+                        NAME
+                    </th>
+                    <th scope="col" class="th">
+                        EMAIL
+                    </th>
+                    <th scope="col" class="th">
+                        PHONE NUMBER
+                    </th>
+                    <th scope="col" class="th">
+                        ROLE
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="td">
+                        Roger
+                    </td>
+                    <td class="td">
+                        r.sanchez@gmail.com
+                    </td>
+                    <td class="td">
+                        645 54 34 56
+                    </td>
+                    <td class="td">
+                        Employee
+                    </td>
+                    <td class="td">
+                        <button class="edit"><a href="#" >Edit</a></button>
+                    </td>
+                    <td class="td">
+                        <button class="delete"><a href="#" >Delete</a></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="td">
+                        Roger
+                    </td>
+                    <td class="td">
+                        r.sanchez@gmail.com
+                    </td>
+                    <td class="td">
+                        645 54 34 56
+                    </td>
+                    <td class="td">
+                        Employee
+                    </td>
+                    <td class="td">
+                        <button class="edit"><a href="#" >Edit</a></button>
+                    </td>
+                    <td class="td">
+                        <button class="delete"><a href="#" >Delete</a></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="td">
+                        Roger
+                    </td>
+                    <td class="td">
+                        r.sanchez@gmail.com
+                    </td>
+                    <td class="td">
+                        645 54 34 56
+                    </td>
+                    <td class="td">
+                        Employee
+                    </td>
+                    <td class="td">
+                        <button class="edit"><a href="#" >Edit</a></button>
+                    </td>
+                    <td class="td">
+                        <button class="delete"><a href="#" >Delete</a></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="td">
+                        Roger
+                    </td>
+                    <td class="td">
+                        r.sanchez@gmail.com
+                    </td>
+                    <td class="td">
+                        645 54 34 56
+                    </td>
+                    <td class="td">
+                        Employee
+                    </td>
+                    <td class="td">
+                        <button class="edit"><a href="#" >Edit</a></button>
+                    </td>
+                    <td class="td">
+                        <button class="delete"><a href="#" >Delete</a></button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
     <x-footer />
 </body>
 
