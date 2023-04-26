c7d9667c1eab183821366a4a1f3f6cf85143a316
Alec Bulka <a.bulka2dawnuria2022@gmail.com>
2023-04-26 14:24:27 +0000

    finish buy cars page

--- resources/views/buy-cars.blade.php
+++ resources/views/buy-cars.blade.php
@@ -18,8 +18,17 @@
     <div class="container">
         <img class="car" src="/img/main-img1.jpg" alt="">
         <div class="text">
-            <h3>Pre-Owned 2022</h3>
-            <h2>Lamborghini STO</h2>
+            <div class="top">
+                <div>
+                    <h3>Pre-Owned 2022</h3>
+                    <h2>Lamborghini STO</h2>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="none" class="bookmark">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                </svg>
+            </div>
             <hr>
             <div class="seccion">
                 <div class="left-seccion">
@@ -33,7 +42,7 @@
                     <p>Transmission: 7-Speed</p>
                     <p>Engine: 640HP</p>
                     <h3>€250,000.00</h3>
-                    <a class="viewCar" href="#">View Car</a>
+                    <button class="viewCar"><a href="#">View Car</a></button>
                 </div>
             </div>
         </div>
@@ -41,31 +50,17 @@
     <div class="container">
         <img class="car" src="/img/main-img1.jpg" alt="">
         <div class="text">
-            <h3>Pre-Owned 2022</h3>
-            <h2>Lamborghini STO</h2>
-            <hr>
-            <div class="seccion">
-                <div class="left-seccion">
-                    <p>Odometer: 2,500 KM</p>
-                    <p>Exterior: Red</p>
-                    <p>Interior: Black</p>
-                    <h3>Price (EUR)</h3>
+            <div class="top">
+                <div>
+                    <h3>Pre-Owned 2022</h3>
+                    <h2>Lamborghini STO</h2>
                 </div>
-                <div class="right-seccion">
-                    <p>Drivetrain: AWD</p>
-                    <p>Transmission: 7-Speed</p>
-                    <p>Engine: 640HP</p>
-                    <h3>€250,000.00</h3>
-                    <a class="viewCar" href="#">View Car</a>
-                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="none" class="bookmark">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                </svg>
             </div>
-        </div>
-    </div>
-    <div class="container">
-        <img class="car" src="/img/main-img1.jpg" alt="">
-        <div class="text">
-            <h3>Pre-Owned 2022</h3>
-            <h2>Lamborghini STO</h2>
             <hr>
             <div class="seccion">
                 <div class="left-seccion">
@@ -79,30 +74,7 @@
                     <p>Transmission: 7-Speed</p>
                     <p>Engine: 640HP</p>
                     <h3>€250,000.00</h3>
-                    <a class="viewCar" href="#">View Car</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <img class="car" src="/img/main-img1.jpg" alt="">
-        <div class="text">
-            <h3>Pre-Owned 2022</h3>
-            <h2>Lamborghini STO</h2>
-            <hr>
-            <div class="seccion">
-                <div class="left-seccion">
-                    <p>Odometer: 2,500 KM</p>
-                    <p>Exterior: Red</p>
-                    <p>Interior: Black</p>
-                    <h3>Price (EUR)</h3>
-                </div>
-                <div class="right-seccion">
-                    <p>Drivetrain: AWD</p>
-                    <p>Transmission: 7-Speed</p>
-                    <p>Engine: 640HP</p>
-                    <h3>€250,000.00</h3>
-                    <a class="viewCar" href="#">View Car</a>
+                    <button class="viewCar"><a href="#">View Car</a></button>
                 </div>
             </div>
         </div>
