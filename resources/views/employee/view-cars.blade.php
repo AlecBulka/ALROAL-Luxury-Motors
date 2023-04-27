cc8050aea09802005b844e7a0f8e9ab9486de42e
Alec Bulka <a.bulka2dawnuria2022@gmail.com>
2023-04-27 13:50:40 +0000

    fix logo

--- resources/views/employee/view-cars.blade.php
+++ resources/views/employee/view-cars.blade.php
@@ -15,26 +15,7 @@
 
 <body>
     <x-header />
-    <div class="menu">
-        <div class="dashboard">
-            <a href="#">Dashboard</a>
-        </div>
-        <div class="view-cars">
-            <a href="#">View Cars</a>
-        </div>
-        <div class="view-users">
-            <a href="#">View Users</a>
-        </div>
-        <div class="add-car">
-            <a href="#">Add Car</a>
-        </div>
-        <div class="add-user">
-            <a href="#">Add User</a>
-        </div>
-        <div class="buy-cars">
-            <a href="#">Buy Cars</a>
-        </div>
-    </div>
+    <x-header-employee/>
 
     <div class="container">
         <h1 class="title">Users List</h1>
