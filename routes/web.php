90981dbe58c6861b747ccab6745afd6c9b6b8472
Alec Bulka <a.bulka2dawnuria2022@gmail.com>
2023-04-30 10:25:03 +0000

    Fix Pages footer and fix routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Login, Register, Forgot Pasword
+require __DIR__.'/auth.php';
+
+
+//Main Routes
 Route::get('/', function () {
     return view('home');
 });
@@ -22,24 +27,12 @@ Route::get('/buy-cars', function () {
     return view('buy-cars');
 });
 
+Route::get('/car-details', function () {
+    return view('car-details');
+});
+
 Route::get('/sell-cars', function () {
     return view('sell-cars');
-});
-
-Route::get('/view-users', function () {
-    return view('admin/view-users');
-});
-
-Route::get('/view-cars', function () {
-    return view('admin/view-cars');
-});
-
-Route::get('/employee/view-cars', function () {
-    return view('employee/view-cars');
-});
-
-Route::get('/admin-dashboard-add-car', function () {
-    return view('admin-dashboard-add-car');
 });
 
 Route::get('/tunning', function () {
@@ -50,54 +43,71 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('/user-dashboard', function () {
+// User Routes
+Route::get('/user/dashboard', function () {
     return view('user/dashboard');
 });
 
-Route::get('/user-view', function () {
+Route::get('/user/account', function () {
     return view('user/view');
 });
 
-Route::get('/car-details', function () {
-    return view('car-details');
-});
-
-Route::get('/saved-cars', function () {
+Route::get('/user/saved-cars', function () {
     return view('user/saved-cars');
 });
 
-Route::get('/add-user', function () {
-    return view('admin/add-user');
+
+//Employee Routes
+Route::get('/employee/view-cars', function () {
+    return view('employee/view-cars');
 });
 
-Route::get('/edit-user', function () {
-    return view('admin/edit-user');
-});
-
-Route::get('/admin-dashboard-add-car', function () {
-    return view('admin/admin-dashboard-add-car');
-});
-
-Route::get('/admin-dashboard-edit-car', function () {
-    return view('admin/admin-dashboard-edit-car');
-});
-
-Route::get('/employee-dashboard-add-car', function () {
+Route::get('/employee/add-car', function () {
     return view('employee/employee-dashboard-add-car');
 });
 
-Route::get('/employee-dashboard-edit-car', function () {
+Route::get('/employee/edit-car', function () {
     return view('employee/employee-dashboard-edit-car');
 });
 
-require __DIR__.'/auth.php';
+
+//Admin Routes
+Route::get('/admin/view-cars', function () {
+    return view('admin/view-cars');
+});
+
+Route::get('/admin/add-car', function () {
+    return view('admin/admin-dashboard-add-car');
+});
+
+Route::get('/admin/edit-car', function () {
+    return view('admin/admin-dashboard-edit-car');
+});
+
+Route::get('/admin/view-users', function () {
+    return view('admin/view-users');
+});
+
+Route::get('/admin/add-user', function () {
+    return view('admin/add-user');
+});
+
+Route::get('/admin/edit-user', function () {
+    return view('admin/edit-user');
+});
+
+
+
+
+
+
+/*Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');*/
+
+/*Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});*/
