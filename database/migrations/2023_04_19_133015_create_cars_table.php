1a1c57acce6ed47e5a5c88afbf083cfff1afee66
Alec Bulka <a.bulka2dawnuria2022@gmail.com>
2023-04-30 10:29:36 +0000

    add is sold column to cars table

--- database/migrations/2023_04_19_133015_create_cars_table.php
+++ database/migrations/2023_04_19_133015_create_cars_table.php
@@ -31,6 +31,7 @@ return new class extends Migration
             $table->foreignId('carmodel_id');
             $table->foreignId('carbrand_id');
             $table->integer('year');
+            $table->boolean('isSold')->default(false);
             $table->timestamps();
         });
     }
