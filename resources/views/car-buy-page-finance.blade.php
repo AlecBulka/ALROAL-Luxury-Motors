c9d7df8890da5f23641f31317f9c6d86fe367bc5
alexguty42 <a.gutierrez2dawnuria2022@gmail.com>
2023-05-05 16:02:05 +0200

    car-buy-page-finance

--- resources/views/car-buy-page-finance.blade.php
+++ resources/views/car-buy-page-finance.blade.php
@@ -16,7 +16,19 @@
 <body>
     <x-header />
     <div class="container">
-
+        <div class="box1">
+        <h3>2022 Lamborghini STO</h3>
+        <div class="imagen"><img src="/img/main-img1.jpg" alt=""></div>
+        <h4>Overview</h4>
+        <div class="col1">
+            <div class="p1">Transmission</div>
+            <div class="p2">7-Speed</div>
+            <div class="p3">Exterior Color</div>
+            <div class="p4">Red</div>
+            <div class="p5">Drivetrain</div>
+            <div class="p6">AWD</div>
+        </div>
+      </div>
     </div>
     <x-footer />
 </body>
