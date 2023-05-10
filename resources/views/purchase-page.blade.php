8e804065937c8880e4df17831d3a13f31d3ede3d
alexguty42 <a.gutierrez2dawnuria2022@gmail.com>
2023-05-10 16:45:26 +0200

    purchase page html and css

--- resources/views/purchase-page.blade.php
+++ resources/views/purchase-page.blade.php
@@ -16,7 +16,9 @@
 <body>
     <x-header />
         <div class="box">
-            <img src="/img/main-img1.jpg">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
             <h1>CAR PURCHASED SUCCESFULLY</h1>
             <h3>Your order number is: 00000001</h3>
             <button class="order">VIEW ORDER</button>
