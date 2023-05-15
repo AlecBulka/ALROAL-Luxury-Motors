d090306ca43599a56cd7332825921f6e681607d8
Alec Bulka <a.bulka2dawnuria2022@gmail.com>
2023-05-15 13:45:22 +0000

    add favicon to all pages

--- resources/views/user/account.blade.php
+++ resources/views/user/account.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Luxury Motors</title>
-
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/svg" href="{{ asset('img/logo/logo-color.svg') }}" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
